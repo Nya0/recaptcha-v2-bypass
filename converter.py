85d1885a71cb7e96c1ac596f0df0154614725483
Sadtrxsh <65015376+Sadtrxsh@users.noreply.github.com>
2020-07-12 13:52:19 -0500

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 from pydub import AudioSegment
 
 def convert():
-	    sound = AudioSegment.from_mp3("./Downloads/audio.mp3")
-	    sound.export("./Downloads/audio.wav", format="wav") 
+	    sound = AudioSegment.from_mp3("~/Downloads/audio.mp3")
+	    sound.export("~/Downloads/audio.wav", format="wav") 
